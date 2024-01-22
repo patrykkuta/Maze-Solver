e4980725676a08067e23ca86309195be7c3f3126
patryk9837 <patryk9837@gmail.com>
2024-01-22 14:26:02 +0000

    Added destructor implementation

--- MazeSolver/breadthfirstsearch.cpp
+++ MazeSolver/breadthfirstsearch.cpp
@@ -1,7 +1,7 @@
 #include "breadthfirstsearch.h"
 
 BreadthFirstSearch::BreadthFirstSearch(vector<array<int, 2>> path) : Algorithm(path) {}
-
+BreadthFirstSearch::~BreadthFirstSearch() {}
 vector<array<int, 2>> BreadthFirstSearch::solve(Maze maze) {
     vector<array<int, 2>> v;
     array<int, 2> a = {1, 2};
--- MazeSolver/depthfirstsearch.cpp
+++ MazeSolver/depthfirstsearch.cpp
@@ -1,7 +1,7 @@
 #include "depthfirstsearch.h"
 
 DepthFirstSearch::DepthFirstSearch(vector<array<int, 2>> path) : Algorithm(path){}
-
+DepthFirstSearch::~DepthFirstSearch() {}
 vector<array<int, 2>> DepthFirstSearch::solve(Maze maze) {
     vector<array<int, 2>> v;
     array<int, 2> a = {1, 2};
