373cfc1de4bbd79dae33e881bbff0c68976076dc
patryk9837 <patryk9837@gmail.com>
2024-01-22 14:27:19 +0000

    Added a default constructor

--- MazeSolver/customisablemaze.cpp
+++ MazeSolver/customisablemaze.cpp
@@ -1,3 +1,4 @@
 #include "customisablemaze.h"
 
 CustomisableMaze::CustomisableMaze(int width, int height, double complexity) : Maze(width, height, complexity) {}
+CustomisableMaze::CustomisableMaze() : Maze() {}
