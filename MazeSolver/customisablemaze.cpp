bedb94a5a4ca391f83cae9d4d6babd40e95b21aa
patryk9837 <patryk9837@gmail.com>
2024-01-24 11:47:56 +0000

    Removed complexity variable

--- MazeSolver/customisablemaze.cpp
+++ MazeSolver/customisablemaze.cpp
@@ -1,4 +1,4 @@
 #include "customisablemaze.h"
 
-CustomisableMaze::CustomisableMaze(unsigned short width, unsigned short height, double complexity) :
-    Maze(width, height, complexity) {}
+CustomisableMaze::CustomisableMaze(unsigned short width, unsigned short height) :
+    Maze(width, height) {}
--- MazeSolver/customisablemaze.h
+++ MazeSolver/customisablemaze.h
@@ -6,13 +6,12 @@
 class CustomisableMaze : public Maze
 {
 public:
-    CustomisableMaze(unsigned short width, unsigned short height, double complexity);
+    CustomisableMaze(unsigned short width, unsigned short height);
 
     bool addWallCoords(int x, int y);
     bool addPathCoords(int x, int y);
     void setWidth(unsigned short width);
     void setHeight(unsigned short height);
-    void setComplexity(double complexity);
     void setStartCoords(int x, int y);
     void setFinishCoords(int x, int y);
 };
