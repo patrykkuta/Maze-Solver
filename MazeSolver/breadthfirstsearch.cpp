e0aaa7b2d10f64463123757d4a9028ad59ad7d30
patryk9837 <patryk9837@gmail.com>
2024-01-24 11:46:22 +0000

    Implemented path finding with BFS algorithm

--- MazeSolver/breadthfirstsearch.cpp
+++ MazeSolver/breadthfirstsearch.cpp
@@ -1,10 +1,94 @@
 #include "breadthfirstsearch.h"
+#include <queue>
+#include <iostream>
+#include <set>
 
-BreadthFirstSearch::BreadthFirstSearch(vector<Cell> path) : Algorithm(path) {}
+using namespace std;
+
+BreadthFirstSearch::BreadthFirstSearch() {}
+
 BreadthFirstSearch::~BreadthFirstSearch() {}
-vector<Cell> BreadthFirstSearch::solve(Maze maze) {
-    vector<Cell> v;
-    Cell c(10, 20);
-    v.push_back(c);
-    return v;
+
+vector<Cell*> BreadthFirstSearch::solve(Maze &maze) {
+
+    for(vector<Cell*> col: maze.getMaze()) {
+        for (Cell* cell: col) {
+            cell->resetVisited();
+        }
+    }
+
+    queue<Cell*> q;
+    unordered_map<Cell*, Cell*> path;
+
+    q.push(maze.getStartCell());
+
+    while (!q.empty()) {
+        Cell* current = q.front();
+        q.pop();
+
+        // Check if the current cell is the finish cell
+        if (*current == *maze.getFinishCell()) {
+            return backtrace(path, maze.getStartCell(), maze.getFinishCell());
+        }
+
+        current->visit();
+
+        Wall walls[] = {Wall::NONE, Wall::NONE, Wall::NONE, Wall::NONE};
+        for (Wall wall: current->getWalls()) {
+            if (wall == Wall::NORTH) {
+                walls[0] = Wall::NORTH;
+            }
+            else if (wall == Wall::SOUTH) {
+                walls[1] = Wall::SOUTH;
+            }
+            else if (wall == Wall::EAST) {
+                walls[2] = Wall::EAST;
+            }
+            else if (wall == Wall::WEST) {
+                walls[3] = Wall::WEST;
+            }
+        }
+
+        // North
+        if(walls[0] == Wall::NONE && !maze.getMaze()[current->getY() - 1][current->getX()]->wasVisited()) {
+            q.push(maze.getMaze()[current->getY() - 1][current->getX()]);
+            path.insert(pair<Cell*, Cell*>(maze.getMaze()[current->getY() - 1][current->getX()], current));
+        }
+
+        // South
+        if(walls[1] == Wall::NONE && !maze.getMaze()[current->getY() + 1][current->getX()]->wasVisited()) {
+            q.push(maze.getMaze()[current->getY() + 1][current->getX()]);
+            path.insert(pair<Cell*, Cell*>(maze.getMaze()[current->getY() + 1][current->getX()], current));
+        }
+
+        // East
+        if(walls[2] == Wall::NONE && !maze.getMaze()[current->getY()][current->getX() + 1]->wasVisited()) {
+            q.push(maze.getMaze()[current->getY()][current->getX() + 1]);
+            path.insert(pair<Cell*, Cell*>(maze.getMaze()[current->getY()][current->getX() + 1], current));
+        }
+
+        // West
+        if(walls[3] == Wall::NONE && !maze.getMaze()[current->getY()][current->getX() - 1]->wasVisited()) {
+            q.push(maze.getMaze()[current->getY()][current->getX() - 1]);
+            path.insert(pair<Cell*, Cell*>(maze.getMaze()[current->getY()][current->getX() - 1], current));
+        }
+    }
+
+    return *new vector<Cell*>();
+}
+
+vector<Cell*> BreadthFirstSearch::backtrace(unordered_map<Cell*, Cell*> &path, Cell* startCell, Cell* finishCell) {
+    Cell* current = finishCell;
+    vector<Cell*> backtracedPath;
+
+    while (*current != *startCell) {
+        if(path.find(current) != path.end()) {
+            backtracedPath.push_back(path[current]);
+            current = path[current];
+        }
+    }
+
+    backtracedPath.pop_back(); // Remove starting cell
+
+    return backtracedPath;
 }
--- MazeSolver/breadthfirstsearch.h
+++ MazeSolver/breadthfirstsearch.h
@@ -2,14 +2,16 @@
 #define BREADTHFIRSTSEARCH_H
 
 #include "algorithm.h"
+#include <unordered_map>
 
 class BreadthFirstSearch : public Algorithm
 {
 public:
-    BreadthFirstSearch(vector<Cell> path);
+    BreadthFirstSearch();
     ~BreadthFirstSearch();
 
-    vector<Cell> solve(Maze maze);
+    vector<Cell*> solve(Maze &maze);
+    vector<Cell*> backtrace(unordered_map<Cell*, Cell*> &path, Cell* startCell, Cell* finishCell);
 };
 
 #endif // BREADTHFIRSTSEARCH_H
