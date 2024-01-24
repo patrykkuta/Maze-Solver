a16e854520e97b0cbfdb7ab3f52f9e2d00c0ee20
patryk9837 <patryk9837@gmail.com>
2024-01-24 18:20:05 +0000

    Added pen size

--- MazeSolver/customrectitem.cpp
+++ MazeSolver/customrectitem.cpp
@@ -23,25 +23,26 @@ void CustomRectItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *op
     painter->setPen(Qt::NoPen);
 
     QColor penColor = QApplication::palette().color(QPalette::WindowText);
+    int penSize = 3;
 
     // Draw the rectangle without specific borders
     painter->drawRect(rect());
 
     for (Wall wall: cell.getWalls()) {
         if (wall == Wall::NORTH) {
-            painter->setPen(penColor); // Set the color for the top border
+            painter->setPen(QPen(penColor, penSize)); // Set the color for the top border
             painter->drawLine(rect().topLeft(), rect().topRight());
         }
         else if (wall == Wall::SOUTH) {
-            painter->setPen(penColor); // Set the color for the bottom border
+            painter->setPen(QPen(penColor, penSize)); // Set the color for the bottom border
             painter->drawLine(rect().bottomLeft(), rect().bottomRight());
         }
         else if (wall == Wall::WEST) {
-            painter->setPen(penColor); // Set the color for the left border
+            painter->setPen(QPen(penColor, penSize)); // Set the color for the left border
             painter->drawLine(rect().topLeft(), rect().bottomLeft());
         }
         else if (wall == Wall::EAST) {
-            painter->setPen(penColor); // Set the color for the right border
+            painter->setPen(QPen(penColor, penSize)); // Set the color for the right border
             painter->drawLine(rect().topRight(), rect().bottomRight());
         }
     }
