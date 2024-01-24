db73557a828cd76adafde5b1b59f1cd4223d1e74
patryk9837 <patryk9837@gmail.com>
2024-01-24 11:52:41 +0000

    Parameterized constructor -> Default

--- MazeSolver/depthfirstsearch.cpp
+++ MazeSolver/depthfirstsearch.cpp
@@ -1,10 +1,10 @@
 #include "depthfirstsearch.h"
 
-DepthFirstSearch::DepthFirstSearch(vector<Cell> path) : Algorithm(path){}
+DepthFirstSearch::DepthFirstSearch(){}
 DepthFirstSearch::~DepthFirstSearch() {}
-vector<Cell> DepthFirstSearch::solve(Maze maze) {
-    vector<Cell> v;
+vector<Cell*> DepthFirstSearch::solve(Maze maze) {
+    vector<Cell*> v;
     Cell c(10, 20);
-    v.push_back(c);
+    v.push_back(&c);
     return v;
 }
--- MazeSolver/depthfirstsearch.h
+++ MazeSolver/depthfirstsearch.h
@@ -8,10 +8,10 @@ using namespace std;
 class DepthFirstSearch : public Algorithm
 {
 public:
-    DepthFirstSearch(vector<Cell> path);
+    DepthFirstSearch();
     ~DepthFirstSearch();
 
-    vector<Cell> solve(Maze maze);
+    vector<Cell*> solve(Maze maze);
 };
 
 #endif // DEPTHFIRSTSEARCH_H
