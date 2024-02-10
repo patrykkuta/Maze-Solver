c3921a3ad17c6bf6bdd711103955024d8cab93d5
patryk9837 <patryk9837@gmail.com>
2024-02-10 15:51:09 +0000

    Move Prims Algorithm implementation to own class

--- MazeSolver/randomizedmaze.h
+++ MazeSolver/randomizedmaze.h
@@ -2,26 +2,13 @@
 #define RANDOMIZEDMAZE_H
 
 #include "maze.h"
-#include <algorithm>
-#include <random>
-#include <set>
 
 using namespace std;
 
 class RandomizedMaze : public Maze
 {
-private:
-    set<Cell*> frontier;
-    random_device rd;
-
-    Cell* getGenerationStartCell();
-    void addUnvisitedNeighbours(Cell* cell);
-    unsigned short randomFrontierPosition();
-    bool connect(Cell* cell);
-
-
 public:
-    RandomizedMaze(unsigned short width, unsigned short height);
+    RandomizedMaze(unsigned short width, unsigned short height, GeneratingAlgorithm* algorithm);
     ~RandomizedMaze();
 
     void generateMaze() override;
