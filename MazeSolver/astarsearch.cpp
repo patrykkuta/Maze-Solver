d71380df290804eb29b905c4bc3f766229d0e5e6
patryk9837 <patryk9837@gmail.com>
2024-02-12 23:35:56 +0000

    Implemented abstract addUnvisitedNeighbours

--- MazeSolver/astarsearch.cpp
+++ MazeSolver/astarsearch.cpp
@@ -1,76 +1,74 @@
-/**
- * @file AStarSearch.cpp
- * @brief Implementation of the AStarSearch class methods.
- */
-
 #include "astarsearch.h"
 
 AStarSearch::AStarSearch() {}
 
 AStarSearch::~AStarSearch() {}
 
-void AStarSearch::solve(Maze &maze) {
+void AStarSearch::solve(Maze* maze) {
 
-    for(vector<Cell*>& col: maze.getMaze()) {
+    this->maze = maze;
+
+    for(vector<Cell*>& col: maze->getMaze()) {
         for (Cell* cell: col) {
             cell->resetVisited();
         }
     }
 
-    priority_queue<pair<Cell*, int>, vector<pair<Cell*, int>>, Comparator> frontier;
-    unordered_map<Cell*, Cell*> path;
-
-    Cell* startCell = maze.getStartCell();
-    Cell* finishCell = maze.getFinishCell();
+    startCell = maze->getStartCell();
+    finishCell = maze->getFinishCell();
     frontier.push(pair<Cell*, int>(startCell, manhattanDistance(startCell, finishCell)));
 
     while (!frontier.empty()) {
         Cell* current = frontier.top().first;
         frontier.pop();
 
-        solvingSteps.push(new Step(State::CURRENT, current));
+        steps.push_back(Step(State::CURRENT, current));
 
         // Check if the current cell is the finish cell
-        if (*current == *maze.getFinishCell()) {
-            solution = backtrace(path, maze.getStartCell(), maze.getFinishCell());
+        if (*current == *maze->getFinishCell()) {
+            solution = backtrace(path, maze->getStartCell(), maze->getFinishCell());
             break;
         }
 
         current->visit();
 
-        map<Wall, bool> walls = current->getWalls();
-
-        // North
-        if(!walls[Wall::NORTH] && !maze.getMaze()[current->getY() - 1][current->getX()]->wasVisited()) {
-            frontier.push(pair<Cell*, int>(maze.getMaze()[current->getY() - 1][current->getX()], manhattanDistance(maze.getMaze()[current->getY() - 1][current->getX()], finishCell)));
-            path.insert(pair<Cell*, Cell*>(maze.getMaze()[current->getY() - 1][current->getX()], current));
-            solvingSteps.push(new Step(State::NEIGHBOUR, maze.getMaze()[current->getY() - 1][current->getX()]));
-        }
-
-        // South
-        if(!walls[Wall::SOUTH] && !maze.getMaze()[current->getY() + 1][current->getX()]->wasVisited()) {
-            frontier.push(pair<Cell*, int>(maze.getMaze()[current->getY() + 1][current->getX()], manhattanDistance(maze.getMaze()[current->getY() + 1][current->getX()], finishCell)));
-            path.insert(pair<Cell*, Cell*>(maze.getMaze()[current->getY() + 1][current->getX()], current));
-            solvingSteps.push(new Step(State::NEIGHBOUR, maze.getMaze()[current->getY() + 1][current->getX()]));
-        }
-
-        // East
-        if(!walls[Wall::EAST] && !maze.getMaze()[current->getY()][current->getX() + 1]->wasVisited()) {
-            frontier.push(pair<Cell*, int>(maze.getMaze()[current->getY()][current->getX() + 1], manhattanDistance(maze.getMaze()[current->getY()][current->getX() + 1], finishCell)));
-            path.insert(pair<Cell*, Cell*>(maze.getMaze()[current->getY()][current->getX() + 1], current));
-            solvingSteps.push(new Step(State::NEIGHBOUR, maze.getMaze()[current->getY()][current->getX() + 1]));
-        }
-
-        // West
-        if(!walls[Wall::WEST] && !maze.getMaze()[current->getY()][current->getX() - 1]->wasVisited()) {
-            frontier.push(pair<Cell*, int>(maze.getMaze()[current->getY()][current->getX() - 1], manhattanDistance(maze.getMaze()[current->getY()][current->getX() - 1], finishCell)));
-            path.insert(pair<Cell*, Cell*>(maze.getMaze()[current->getY()][current->getX() - 1], current));
-            solvingSteps.push(new Step(State::NEIGHBOUR, maze.getMaze()[current->getY()][current->getX() - 1]));
-        }
+        addUnvisitedNeighbours(current);
     }
 }
 
 unsigned int AStarSearch::manhattanDistance(Cell* current, Cell* finishCell) {
     return (abs(current->getX() - finishCell->getX()) + abs(current->getY() - finishCell->getY()));
+}
+
+void AStarSearch::addUnvisitedNeighbours(Cell* cell) {
+    map<Wall, bool> walls = cell->getWalls();
+
+    // North
+    if(!walls[Wall::NORTH] && !maze->getMaze()[cell->getY() - 1][cell->getX()]->wasVisited()) {
+        frontier.push(pair<Cell*, int>(maze->getMaze()[cell->getY() - 1][cell->getX()], manhattanDistance(maze->getMaze()[cell->getY() - 1][cell->getX()], finishCell)));
+        path.insert(pair<Cell*, Cell*>(maze->getMaze()[cell->getY() - 1][cell->getX()], cell));
+        steps.push_back(Step(State::NEIGHBOUR, maze->getMaze()[cell->getY() - 1][cell->getX()]));
+    }
+
+    // South
+    if(!walls[Wall::SOUTH] && !maze->getMaze()[cell->getY() + 1][cell->getX()]->wasVisited()) {
+        frontier.push(pair<Cell*, int>(maze->getMaze()[cell->getY() + 1][cell->getX()], manhattanDistance(maze->getMaze()[cell->getY() + 1][cell->getX()], finishCell)));
+        path.insert(pair<Cell*, Cell*>(maze->getMaze()[cell->getY() + 1][cell->getX()], cell));
+        steps.push_back(Step(State::NEIGHBOUR, maze->getMaze()[cell->getY() + 1][cell->getX()]));
+    }
+
+    // East
+    if(!walls[Wall::EAST] && !maze->getMaze()[cell->getY()][cell->getX() + 1]->wasVisited()) {
+        frontier.push(pair<Cell*, int>(maze->getMaze()[cell->getY()][cell->getX() + 1], manhattanDistance(maze->getMaze()[cell->getY()][cell->getX() + 1], finishCell)));
+        path.insert(pair<Cell*, Cell*>(maze->getMaze()[cell->getY()][cell->getX() + 1], cell));
+        steps.push_back(Step(State::NEIGHBOUR, maze->getMaze()[cell->getY()][cell->getX() + 1]));
+    }
+
+    // West
+    if(!walls[Wall::WEST] && !maze->getMaze()[cell->getY()][cell->getX() - 1]->wasVisited()) {
+        frontier.push(pair<Cell*, int>(maze->getMaze()[cell->getY()][cell->getX() - 1], manhattanDistance(maze->getMaze()[cell->getY()][cell->getX() - 1], finishCell)));
+        path.insert(pair<Cell*, Cell*>(maze->getMaze()[cell->getY()][cell->getX() - 1], cell));
+        getSteps()->push_back(Step(State::NEIGHBOUR, maze->getMaze()[cell->getY()][cell->getX() - 1]));
+    }
 }
 
